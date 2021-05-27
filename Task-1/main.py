4b119adfed126485e59f6619117d0a34a2379d59
GitNegi <66105456+GitNegi@users.noreply.github.com>
2021-05-27 09:20:23 +0530

    Add files via upload

---
+++ Task-1/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv("salarydata.csv")
+
+X = dataset['YearsExperience'].values.reshape(-1,1)
+y = dataset['Salary']
+
+from sklearn.linear_model import LinearRegression
+brain = LinearRegression()
+brain.fit(X,y)
+x = brain.predict([[float(input("Enter Years of Experience :->"))]])
+print(x)
